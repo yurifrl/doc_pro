e4d2d10f607d879c0245a7dd68a241c3a95045da
yurifrl <yurifrl@hotmail.com>
2014-10-24 09:21:53 -0200

    new things

--- app/admin/tag.rb
+++ app/admin/tag.rb
@@ -1,17 +1,27 @@
 ActiveAdmin.register Tag do
   before_filter :skip_sidebar!, :only => :index
 
+
+  action_item do
+    link_to "Add", "/admin/tags/new"
+  end
+
+
   permit_params :name, :tag_name, :description, :example, :synopsis, :version, page_ids: [], contexts_ids: [], tag_ids: [], block_ids: []
 
   form do |f|
     f.actions
-    f.inputs do
-      f.input :pages, as: :select2_multiple, :input_html => {:multiple => true}
-      f.input :contexts, as: :select2_multiple, :input_html => {:multiple => true}
-      f.input :blocks, as: :select2_multiple, label: 'Pertence ao Bloco', :input_html => {:multiple => true}, collection: Tag.all.pluck(:tag_name, :id)
-      f.input :tags, as: :select2_multiple, label: 'Possui as Tags', :input_html => {:multiple => true}, collection: Tag.all.pluck(:tag_name, :id)
+    f.inputs "Details" do
       f.input :tag_name
       f.input :name
+    end
+    f.inputs "Location" do
+      f.input :blocks, as: :select2_multiple, label: 'Pertence ao Bloco', :input_html => {:multiple => true}, collection: Tag.all.pluck(:tag_name, :id)
+      f.input :tags, as: :select2_multiple, label: 'Possui as Tags', :input_html => {:multiple => true}, collection: Tag.all.pluck(:tag_name, :id)
+      f.input :pages, as: :select2_multiple, :input_html => {:multiple => true}
+      f.input :contexts, as: :select2_multiple, :input_html => {:multiple => true}
+    end
+    f.inputs "Content" do
       f.input :description
       f.input :example
       f.input :synopsis
@@ -19,5 +29,4 @@ ActiveAdmin.register Tag do
     end
     f.actions
   end
-
 end
